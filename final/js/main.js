c3a5a9e963d530eeb21b92c148acf9a7b1d1dd8b
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-06-14 21:35:14 -0400

    :arrow_up_small: Add h1 / logo animation on mobile
    Added animation to h1 and logo on mobile media query that hides h1 and then shows logo animating in.

--- final/js/main.js
+++ final/js/main.js
@@ -21,6 +21,7 @@
 //   aud_cont.classList.remove('expanded');
 // });
 
+// ! Speaker Toggle
 var speaker = document.getElementById("speaker");
 var mute = document.getElementById("cross_out");
 var cir_btm = document.getElementById("cir_btm");
@@ -36,12 +37,50 @@ speaker.addEventListener('mousedown', () => {
   if (cross_out.classList.contains("off")) {
     cross_out.classList.remove("off");
     cross_out.classList.add("on");
+    cir_btm.classList.remove("stop");
+    cir_btm.classList.add("play");
     audioPlayer.play();
     sndPlaying = true;
   } else {
     cross_out.classList.remove("on");
     cross_out.classList.add("off");
+    cir_btm.classList.remove("play");
+    cir_btm.classList.add("stop");
     audioPlayer.pause();
     sndPlaying = false;
   }
 }, false);
+
+// ! Band Name and Logo Animation
+var bandName = document.getElementById("band_name");
+var logo = document.getElementById("logo");
+
+function nameLogo_mobile(x) {
+  if (x.matches) { // Executes on mobile
+    bandName.addEventListener('mousedown', () => {
+      if (bandName.classList.contains("tap")) {
+        logo.classList.remove("mobile");
+        console.log(logo);
+        setTimeout(() => {
+          bandName.classList.remove("hidden");
+          bandName.classList.remove("tap");
+          console.log(bandName);
+        }, 300);
+      } else {
+        bandName.classList.add("tap");
+        console.log(bandName);
+        setTimeout(() => {
+          bandName.classList.add("hidden");
+          logo.classList.add("mobile");
+          console.log(logo);
+        }, 300);
+      }
+    }, false);
+  } else { // Executes on tablet and larger
+
+  }
+}
+
+var windowWidth = window.matchMedia("(max-width: 48rem)");
+nameLogo_mobile(windowWidth);
+windowWidth.addListener(nameLogo_mobile);
